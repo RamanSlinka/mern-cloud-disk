0946b031b94c21fd693f4525f1eaeda972469111
RamanSlinka <romuch1986@gmail.com>
2022-04-21 21:58:25 +0200

    added state : redux (+redux-thunk);

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-   working
-    </div>
-  );
+    return (
+        <div className="App">
+            working
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Provider} from "react-redux";
+import {store} from "./reducers";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-    <App />
-
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
 
 
