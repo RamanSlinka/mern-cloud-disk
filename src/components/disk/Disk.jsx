9a043945557344060705f8b781d600242dfca441
RamanSlinka <romuch1986@gmail.com>
2022-04-26 23:11:59 +0200

    Added folder navigation

--- src/components/disk/Disk.jsx
+++ src/components/disk/Disk.jsx
@@ -1,18 +1,17 @@
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import { getFiles} from "../../actions/files";
+import {getFiles} from "../../actions/files";
 import FileList from "./fileList/FileList";
 import './disk.css'
 import Popup from "./Popup";
-import {setPopupDisplay} from "../../reducers/fileReducer";
+import {setCurrentDir, setPopupDisplay} from "../../reducers/fileReducer";
 
 
 const Disk = () => {
 
     const dispatch = useDispatch();
     const currentDir = useSelector(state => state.files.currentDir)
-
-
+    const dirStack = useSelector(state => state.files.dirStack)
 
 
     useEffect(() => {
@@ -22,10 +21,19 @@ const Disk = () => {
     function showPopupHandler() {
         dispatch(setPopupDisplay('Flex'))
     }
+
+    function backClickHandler() {
+        const backDirId = dirStack.pop()
+        dispatch(setCurrentDir(backDirId))
+    }
+
     return (
         <div className="disk">
             <div className="disk__btns">
-                <button className="disk__back">Назад</button>
+                <button className="disk__back"
+                        onClick={() => backClickHandler()}
+                >Назад
+                </button>
                 <button className="disk__create"
                         onClick={() => showPopupHandler()}
                 >Создать папку
--- src/components/disk/fileList/file/File.jsx
+++ src/components/disk/fileList/file/File.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import {FcFile, FcOpenedFolder} from "react-icons/fc";
 import './file.css'
+import {pushToStack, setCurrentDir} from "../../../../reducers/fileReducer";
+import {useDispatch, useSelector} from "react-redux";
 
 const File = ({file}) => {
+    const dispatch = useDispatch();
+    const currentDir = useSelector(state => state.files.currentDir)
+
+    function openDirHandler () {
+        dispatch(pushToStack(currentDir))
+        dispatch(setCurrentDir(file._id))
+    }
     return (
-        <div className='file'>
+        <div className='file'
+        onClick={file.type === 'dir' ? ()=>openDirHandler() : ''}
+        >
            {file.type === 'dir' ? <FcOpenedFolder className='file__img'/> : <FcFile className='file__img'/>}
             <div className="file__name">{file.name}</div>
             <div className="file__date">{file.date.slice(0,10)}</div>
