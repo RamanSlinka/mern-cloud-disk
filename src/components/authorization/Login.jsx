8d2ed73571718bb69e560b2633bdf2eb42a236c0
RamanSlinka <romuch1986@gmail.com>
2022-05-12 22:43:44 +0200

    Migration to  module.scss; Updated and changed Login form

--- src/components/authorization/Login.jsx
+++ src/components/authorization/Login.jsx
@@ -1,26 +1,85 @@
 import React, {useState} from 'react';
-import './authorization.css'
+import style from './authorization.module.scss'
 import Input from "../input/Input";
 import {useDispatch} from "react-redux";
 import {login} from "../../actions/user";
+import Registration from "./Registration";
 
 const Login = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const dispatch = useDispatch()
+    const [registration, setRegistration] = useState(false);
+
+const loginClickHandler = () => {
+    dispatch(login(email, password))
+    setEmail('')
+    setPassword('')
+}
+
     return (
-        <div className='authorization'>
-            <div className='registration__header'>Sign in</div>
-            <Input value={email} setValue={setEmail}
-                   type='text' placecholder='Enter email'/>
-            <Input value={password} setValue={setPassword}
-                   type='password' placecholder='Enter password'/>
-            <button
-                className='authorization__btn'
-                onClick={() => dispatch(login(email, password))}
-            >Sign In
-            </button>
+        <div className={style.body}>
+
+
+            <div
+                className={
+                    registration ?
+                        `${style.container} ${style.rightPanelActive}`
+                        : style.container
+                }
+            >
+
+
+                {/*Sign In*/}
+                {!registration
+                    ?
+                    <div className={`${style.containerForm} ${style.containerSignin}`}>
+                        <form action="#" className={style.form}>
+                            <h2 className={style.formTitle}>Sign In</h2>
+                            <Input
+                                value={email}
+                                setValue={setEmail}
+                                type="email" placeholder="Email" className={style.input}/>
+                            <Input
+                                value={password}
+                                setValue={setPassword}
+                                type="password" placeholder="Password" className={style.input}/>
+                            <button
+                                onClick={() => loginClickHandler()}
+                                className={style.btn}>Sign In
+                            </button>
+                        </form>
+                    </div>
+                    :
+                    <Registration/>
+                }
+
+
+                {/*Overlay*/}
+                <div className={style.containerOverlay}>
+                    <div className={style.overlay}>
+                        <div className={`${style.overlayPanel} ${style.overlayLeft}`}>
+                            <button
+                                onClick={() => {
+                                    setRegistration(false)
+                                }}
+                                className={style.btn} id="signIn">Sign In
+                            </button>
+                        </div>
+                        <div className={`${style.overlayPanel} ${style.overlayRight}`}>
+                            <button
+                                onClick={() => {
+                                    setRegistration(true)
+                                }}
+                                className={style.btn} id="signUp">Sign Up
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     );
 };
--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import style from "../authorization/authorization.module.scss";
 
 const Input = (props) => {
     return (
         <input
+            className={style.input}
             onChange={(event) => props.setValue(event.target.value)}
             value={props.value}
             type={props.type}
