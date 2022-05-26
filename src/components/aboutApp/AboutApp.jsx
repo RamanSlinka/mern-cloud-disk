0d74cb3b38831583b248e82c57c78b1a39e1bd36
RamanSlinka <romuch1986@gmail.com>
2022-05-26 22:42:34 +0200

    created AboutApp component

---
+++ src/components/aboutApp/AboutApp.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const AboutApp = () => {
+    return (
+        <div className="content__about ">
+            <h1 id="text" >Hello there !</h1>
+            <div>
+
+            </div>
+            <h3>Pizza Shop</h3>
+            <span>ver.2.0.42</span>
+            <h3>This application is made using technologies such as:</h3>
+            <ul>
+                <li>Hooks: useState, useEffect, useContext. Custom hook</li>
+                <li>React router. Page navigation. BrowserRouter, Route.</li>
+                <li>Typescript.</li>
+                <li>Redux, react-redux, redux-thunk.</li>
+                <li>Search. Filtration.</li>
+                <li>Working with the server. Axios. Indication of loading data from the server.</li>
+                <li>Using "JSON" as BackEnd</li>
+                <li>Loader component.React Skeleton, react-content-loader.</li>
+                <li>SCSS. CSS Animations.Library React icons.</li>
+                <li>  CSS Animations.React-awesome-reveal, Library React icons.</li>
+                <li>React Bootstrap. Slider.</li>
+                <li>Gsap.</li>
+                <li>Classnames.</li>
+            </ul>
+
+        </div>
+
+
+    );
+};
+
+export default AboutApp;
