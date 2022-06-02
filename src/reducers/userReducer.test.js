5fdcfa34c767223151b2b0706824f9ebf3fb3269
RamanSlinka <romuch1986@gmail.com>
2022-06-02 23:14:22 +0200

    Added unit tests for project (Jest). Created userReducer.test.js

--- src/reducers/userReducer.test.js
+++ src/reducers/userReducer.test.js
@@ -1,18 +1,37 @@
 import userReducer, {setUser, logout} from './userReducer';
 
+const user = {
+    email: "test@gmail.com",
+    id: "23",
+    avatar: "ea7d4ddd-7256-441e-abfe-6410fb3a32f1.jpg",
+    diskSpace: 10737418240,
+    usedSpace: 362010391
+}
+
 const defaultState =  {
-    currentUser: {},
+    currentUser: user,
     isAuth: false
 }
 
 test('user should be added', () => {
-    let action = setUser()
+    const action = setUser()
     const newState = userReducer(defaultState, action)
 
     expect(newState.isAuth).toBe(true)
 })
+
+test(` should get user's data`, () => {
+    const action = setUser(user)
+    const newState = userReducer(defaultState, action)
+
+    expect(newState.currentUser.email).toBe("test@gmail.com")
+    expect(newState.currentUser.id).toBe("23")
+    expect(newState.currentUser.avatar).not.toBe("avatar")
+})
+
+
 test('user should be removed', () => {
-    let action = logout()
+    const action = logout()
     const newState = userReducer(defaultState, action)
 
     expect(newState.isAuth).toBe(false)
